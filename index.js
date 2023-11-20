88eb601d4ff31ff4f2aab8859a0ddeedf3b4d29e
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-20 11:22:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import cors_proxy from 'cors-anywhere';
-cors_proxy.createServer({}).listen(8080, "0.0.0.0", ()=>{
-    console.log('Running CORS Anywhere');
+cors_proxy.createServer({
+  setHeaders: {
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "Authorization"
+  },
+  httpProxyOptions: {
+    xfwd: false,
+    changeOrigin: true
+  }
+}).listen(8080, "0.0.0.0", () => {
+  console.log('Running CORS Anywhere');
 });
