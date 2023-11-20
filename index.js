e6411769d11419bed5fe6236fe94b2714067c52d
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-20 09:54:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-import cors_proxy from 'cors-anywhere'
-
-// Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '0.0.0.0'
-// Listen on a specific port via the PORT environment variable
-const port = process.env.PORT || 8080
-
-if (!process.env.CORS_PROXY_OPTIONS) {
-    process.env.CORS_PROXY_OPTIONS = JSON.stringify({
-        originWhitelist: [],
-        removeHeaders: ['cookie', 'cookie2'],
-    })
-}
-
-const corsProxyOptions = JSON.parse(process.env.CORS_PROXY_OPTIONS)
-
-cors_proxy.createServer(corsProxyOptions).listen(port, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port)
-})
+import cors_proxy from 'cors-anywhere';
+cors_proxy.createServer({}).listen(8080, "0.0.0.0", ()=>{
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
